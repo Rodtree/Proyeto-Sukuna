5ee906095de5db74fec067a3e42df5c0f8e325da
_ARod <galarzarodrigo60@gmail.com>
2024-08-14 11:16:49 -0800

    when añades htdocs

---
+++ Industria_Papelera_Web/productos.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Productos - Empresa Maderera</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+          <nav class="navbar">
+            <div class="navbar-logo">
+                <img src="logo_papelera.png" alt="Logo de la empresa" class="loguito">
+                   <h1><a href="index.php">ZKRANOVIC</a></h1>
+            </div>
+            <ul class="navbar-links">
+                <li><a href="nosotros.php">Nosotros</a></li>
+                <li><a href="productos.php">Productos</a></li>
+                <li><a href="servicios.php">Servicios</a></li>
+                <li><a href="contacto.php">Contactos</a></li>
+                <li><a href="registro.php">Registrarse</a></li>
+                <li><a href="login.php">Iniciar Sesión</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+    <div class="background">
+
+    <div class="overlay">
+    <main class="content">
+        <section>
+            <h2>Productos</h2>
+            <p>Descubre nuestra línea de productos madereros:</p>
+            <ul>
+                <li>Muebles de madera maciza</li>
+                <li>Pisos de madera laminada</li>
+                <li>Paneles de madera para construcción</li>
+                <li>Accesorios y decoración en madera</li>
+            </ul>
+        </section>
+    </main>
+</div>
+</div>
+</body>
+</html>
